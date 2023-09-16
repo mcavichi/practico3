8f59184914b0670abd98895d18df1d1234c22dcd
mcavichi <mcavichi@gmail.com>
2023-09-16 14:13:02 -0300

    Practico3 finalizado

--- src/components/Result/Result.jsx
+++ src/components/Result/Result.jsx
@@ -7,7 +7,7 @@ const Result = ({ playerScore, computerScore, round, winner }) => {
       <p>Ronda: {round}</p>
       <p>Tu puntaje: {playerScore}</p>
       <p>Puntaje de la Computadora: {computerScore}</p>
-      {winner && <p>Resultado de la rondan: {winner}</p>}
+      {winner && <p>Resultado de la ronda: {winner}</p>}
     </div>
   );
 }
